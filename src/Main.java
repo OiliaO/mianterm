3fdb0489ed093e5989f31c53ae157918b2bb3cf0
OiliaO <s.iliarabee2006@gmail.com>
2025-04-18 22:56:41 +0330

    class Main updated

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,31 @@
+import music.InvalidOperationException;
+import music.Music;
+
+import music.Playlist;
+import music.User;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) throws InvalidOperationException {
+        User user1 = new User("ilia" ,"12345678");
+        User user2 = new User("mehrdad" , "87654321");
+        user1.follow(user1);
+        user2.follow(user1);
+        user1.printFollowers();
+        user1.buyPremium();
+        Music music1 = new Music("hi" , user1);
+        Music music3 = new Music("hi" , user2);
+        Music music2 = new Music("hello" , user1);
+        Music.search("hi");
+        user1.createPlaylist("shab");
+        Playlist shab = new Playlist("shab" ,  user1);
+        shab.addMusic(music1 , "12345678");
+        shab.removeMusic(music1 , "12345678");
+        shab.editTitle("rooz" , "12345678");
+        shab.addMusic(music1 , "12345678");
+        shab.addMusic(music2 , "12345678");
+        shab.addMusic(music3 , "12345678");
+
+        shab.searchInPlaylist("hi");
+        shab.playPlaylist();
     }
 }
